06d8d11e2cfa86a728881ee9dbdc9a88f9aba249
OlegLobko <sledge@ukr.net>
2015-06-27 19:57:37 +0300

    Worked. Before ordering commit

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include "TaskHolder.h"
 #include <fstream>
 #include "HTTPRequestErrorsQueue.h"
+#include <stdlib.h> // atoi
 
 using namespace std;
 using namespace LobKo;
@@ -30,85 +31,33 @@ using std::string;
  * 
  */
 int main(int argc, char** argv) {
-    //    DomainNameResolver resolver;
-    //string hostname("ukr.net");
-    //    string hostname("cnn.com");
-    //    //string hostname("s-R40-R41");
-    //    std::pair<DNSResult, shared_ptr<IPAdress> > pair;
-    //    pair = resolver(hostname);
-    //
-    //    if ( pair.first == LobKo::DNSResult::DNS_RESOLVE_ERROR ) {
-    //        std::cout << "DNS Error occured" << std::endl;
-    //
-    //        return EXIT_FAILURE;
-    //    }
-    //    std::cout << "!!!!" << hex << *(pair.second).get() << std::endl << dec;
-
-    //    string s1("http:");
-    //    string s2("http://");
-    //    string s3("http://ab");
-    //    string s4("http://ab/");
-    //string s5("http://ab:81");
-    //    string s6("http://ab:81/");
-    //    string s7("http://u@ab:81/");
-    //    //    string s8("http://_@ab:11108/");
-    //    string s8("http://_1@ab:23408/");
-    //    string s9("hxttp://_1@ab:23408/sdsd");
-    //    string s10("http://_1:ya.ru@ya.ru:23408/ss");
-    //string s11("hxttp://_1:ya.ru@ebay.com:2a3408/ss");
-    //URL url5_3(s5);
-    //URL& url5 = url5_3;
-    //    URL url6(s6);
-    //    URL url7(s7);
-    //    URL url8(s8);
-    //    URL url9(s9);
-
-    //    //URL url11(s11);
-    //
-    //cerr << url5 << endl;
-    //    cerr << url6 << endl;
-    //    cerr << url7 << endl;
-    //    cerr << url8 << endl;
-    //    cerr << url9 << endl;
-    //    cerr << spUrl10.get() << endl;
-    //
-    //    pair = resolver(spUrl10->getHost());
-    //
-    //    if ( pair.first == LobKo::DNSResult::DNS_RESOLVE_ERROR ) {
-    //        std::cout << "DNS Error occured" << std::endl;
-    //
-    //        return EXIT_FAILURE;
-    //    }
-    //    std::cout << "!!!!" << hex << *(pair.second).get() << std::endl << dec;
-
-
-    //    string s10("http://ya.ru/");
-    //    shared_ptr<URL> spUrl10(new URL(s10));
-    //    shared_ptr<HTTPRequest> request(new HTTPRequest (HTTPRequestType(HTTPRequestType::GET), spUrl10, HTTPRequestProto(HTTPRequestProto::HTTP1_1)));
-    //
-    //    std::cout << "Request String:" << std::endl << request->getResultString() << std::endl;
     ////////////////////////////////////
-    if ( argc < 2 ) {
-        std::cerr << " Usage:\t\t " << argv[0] << " /path/to/taskFile" << std::endl;
+
+    if ( argc < 3 ) {
+        std::cerr << " Usage:\t\t " << argv[0] << "\t    simultaneous_downloads\t" << " /path/to/taskFile" << std::endl;
         return EXIT_FAILURE;
     }
+    const int simultaneous_download_max = 10;
+    int simultaneous_download_number = ::atoi(argv[1]);
+    ;
 
-    std::ifstream dataFile(argv[1]);
-    shared_ptr<TaskHolder> spTaskHolder;
+    if ( simultaneous_download_number <= 0 || simultaneous_download_number > simultaneous_download_max ) {
+        simultaneous_download_number = simultaneous_download_max;
+    }
+
+    std::ifstream dataFile(argv[2]);
     if ( !dataFile.is_open() ) {
-        std::cerr << "File " << argv[1] << " can't be opened" << std::endl;
+        std::cerr << "File \"" << argv[2] << "\" can't be opened." << std::endl;
         return EXIT_FAILURE;
     }
+    shared_ptr<TaskHolder> spTaskHolder = TaskHolderBuilder::build(dataFile);
 
-    spTaskHolder = TaskHolderBuilder::build(dataFile);
-    //shared_ptr<TaskHolder> spTaskHolder; // = TaskHolderBuilder::build();
-    //QueuesMaster qmaster(request);
     QueuesMaster qmaster(spTaskHolder);
-    qmaster.process(5);
+    qmaster.process(simultaneous_download_number);
 
-    HTTPRequestErrorsQueue& eq = *(qmaster.reqErrorsQ().get());
+    HTTPRequestErrorsQueue& eq = *qmaster.reqErrorsQ().get();
     cerr << eq << std::endl;
-    std::cout << "main exit" << endl;
-    return 0;
+
+    return EXIT_SUCCESS;
 }
 
